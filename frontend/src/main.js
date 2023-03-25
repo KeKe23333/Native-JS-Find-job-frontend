2ddbbb15a1661f1123ae5a442dd1394378c61567
Ke Wu <z5158146@ad.unsw.edu.com>
2023-03-25 23:03:46 +0800

    show feed list almost finished

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,4 @@
+
 import { BACKEND_PORT } from './config.js';
 // A helper you may want to use when uploading new images to the server.
 import { fileToDataUrl } from './helpers.js';
@@ -37,9 +38,10 @@ function showRegister() {
 function showJobFound() {
     login.style.display = 'none';
     register.style.display = 'none';
-    jobFound.style.display = 'flex';
+    jobFound.style.display = 'block';
+    fetchJobList()
 }
-/*add event to the register button in register page
+/*======================= register event =======================
 1. checke the Email address is valid or not
 2. check the password and confirm password is the same or not
 */
@@ -96,8 +98,9 @@ submitRegister.addEventListener('click', (event) => {
     }
 })
 
-
-// add event to the Sign in button in login page
+/*
+======================= login event =======================
+*/
 const submitsignIn = document.getElementById('submitSingin');
 submitsignIn.addEventListener('click', () => {
     const payload = {
@@ -107,15 +110,188 @@ submitsignIn.addEventListener('click', () => {
     fetchRequest('auth/login', 'POST', payload,(data)=>{
         localStorage.setItem('token', data.token);
         location.hash = '#job-found';
+        // console.log('the user id is ', data.userId);
+
     });
 })
 
+
+
+/*
+======================= job stuff here =======================
+*/
 //button to log out
 const logOut = document.querySelector('#logOut');
 logOut.addEventListener('click', () => {
     localStorage.removeItem('token');
     location.hash = '#login';
 })
+
+
+
+function getUserInfo(jobId, createrId){
+
+    fetchRequest(`user?userId=${createrId}`, 'GET', null, (data)=>{
+        /* 
+         data {
+            email: "betty@email.com"
+            id: 26994
+            jobs: array[{job},{job}]
+            name: "Betty"
+            watcheeUserIds:array[ , ]
+         }
+        */
+        console.log('the user Name is ', data.name);
+        document.getElementById(`${jobId}-createrName`).innerText ='Poster: '+data.name;
+
+
+    });
+}
+
+
+function getTheRightTime(time){
+    if(new Date(time).toDateString() === new Date().toDateString()){
+        console.log('the time is ', time.slice(11,16));
+       return time.slice(11,16)
+    }else{
+        const yyyy = time.slice(0,4);
+        const mm = time.slice(5,7);
+        const dd = time.slice(8,10);
+        console.log('the date is ', dd +'/' + mm+ '/' + yyyy);
+        return dd +'/' + mm+ '/' + yyyy
+    }
+}
+
+getTheRightTime('2011-10-05T14:48:00.000Z')
+const jobListContainer = document.querySelector('#job-list-container');
+function createAjobCard(data){
+    const colContainer = document.createElement('div');
+    colContainer.classList.add('col');
+
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card', 'shadow-sm');
+
+    const img = document.createElement('img');
+    img.src = data.image;
+    img.style.width = '100%';
+    img.style.height = '225px';
+
+    const jobBody = document.createElement('div');
+    jobBody.classList.add('card-body');
+    //Job title
+    const jobTitle = document.createElement('h2');
+    jobTitle.innerText = data.title;
+
+    const jobCreaterName= document.createElement('p'); 
+    //creater Name
+    jobCreaterName.classList.add('fst-italic')
+    jobCreaterName.innerText = '--';
+    jobCreaterName.id = data.id +'-createrName';
+
+    //job post time
+    const jobPostTime = document.createElement('p');
+    jobPostTime.classList.add('fw-light')
+    jobPostTime.innerText = 'Post Time: ' + getTheRightTime(data.createdAt);
+
+    // job start time
+
+    const jobStartTime = document.createElement('p');
+    jobStartTime.classList.add('fw-light')
+    jobStartTime.innerText = 'Start Time: ' + data.start.slice(0,10);
+
+    //number of Likes
+    const numOfLikes = document.createElement('p');
+
+    //job description
+    const jobDescription = document.createElement('p');
+    jobDescription.innerText = data.description;
+
+
+
+    jobBody.appendChild(jobTitle);
+    jobBody.appendChild(jobCreaterName);
+    jobBody.appendChild(jobPostTime)
+    jobBody.appendChild(jobStartTime);
+    jobBody.appendChild(jobDescription);
+    cardContainer.appendChild(img);
+    cardContainer.appendChild(jobBody);
+    jobListContainer.appendChild(cardContainer);    
+    // update job
+
+    // console.log('the creater id is ', data.creatorId);
+    getUserInfo(data.id, data.creatorId);
+
+    // console.log('the user name is ', userInfo);
+
+
+}
+
+
+
+function fetchJobList(){
+    fetchRequest('job/feed?start=0', 'GET', null, (data)=>{
+        console.log('this user have those watchs with job', data);
+        jobListContainer.textContent =''
+        data.forEach(data => {
+
+            console.log('the user whatched job list is ', data);
+            createAjobCard(data);
+                // console.log('the job list is ', data);
+                /*
+                data format is :
+                        "id": 528491,
+                        "creatorId": 61021,
+                        "title": "COO for cupcake factory",
+                        "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==",
+                        "start": "2011-10-05T14:48:00.000Z",
+                        "description": "Dedicated technical wizard with a passion and interest in human relationships",
+                        "createdAt": "2011-10-05T14:48:00.000Z",
+                        "likes": [
+                        {
+                            "userId": 61021,
+                            "userEmail": "betty@email.com",
+                            "userName": "Betty"
+                        }
+                        ],
+                        "comments": [
+                        {
+                            "userId": 61021,
+                            "userEmail": "betty@email.com",
+                            "userName": "Betty",
+                            "comment": "This is a great opportunity, my email is hello@unsw.edu.au"
+                */
+
+            });
+
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // show the page based on the hash value in the url
 
@@ -144,6 +320,6 @@ function onHashChange() {
 }
 
 // load the page based on the hash value
-window.onhashchange = function(taget, event) {
+window.onhashchange = function() {
     onHashChange();
 }  
--- frontend/src/request.js
+++ frontend/src/request.js
@@ -1,12 +1,19 @@
+
 export const  fetchRequest = (path, method, payload,success) => {
+    
     const postMethod = {
         method: method,
         headers: {
             'Content-type': 'application/json'
-        },
-        body: JSON.stringify(payload)
+        }
     }
-
+    if (method === 'GET'){
+        const token = localStorage.getItem('token')
+        // console.log('the token is ', token);
+        postMethod.headers['Authorization'] = 'Bearer ' + localStorage.getItem('token')
+    }else{
+        postMethod.body = JSON.stringify(payload)
+    }
     fetch('http://localhost:5005/' + path, postMethod)
     .then((Response) => {
         Response.json()
